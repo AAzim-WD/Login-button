57d3dd101e86280cddac132692b444163d38abc4
AAzim-WP <mohammadaazim.ku.2020@gmail.com>
2022-08-11 12:40:25 +0600

    add login form

---
+++ index.js
@@ -0,0 +1,12 @@
+var myForm = document.getElementById("myForm");
+document.getElementById("show").addEventListener("click", function() {
+   myForm.style.display = "block";
+})
+
+ document.getElementById("cancel-1").addEventListener("click", function() {
+    myForm.style.display = "none";
+ })
+
+ document.getElementById("cancel-2").addEventListener("click", function() {
+    myForm.style.display = "none";
+ })
